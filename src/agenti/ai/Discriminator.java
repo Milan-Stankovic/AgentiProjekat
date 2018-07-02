49ded24a362fd04fde3c06cafe51f6017e748652
ilijagrbic <ilijagrb.ns@gmail.com>
2018-07-02 08:26:15 +0200

    Deo za diskriminator sam bukvalno kopirao tvoj generator, nsiam siguran za neke stvari...

---
+++ src/agenti/ai/Discriminator.java
@@ -0,0 +1,241 @@
+package agenti.ai;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ejb.EJB;
+
+import dto.ContractNetDTO;
+import jms.JMSQueue;
+import model.ACLPoruka;
+import model.AID;
+import model.Agent;
+import model.Baza;
+import model.Performative;
+
+public class Discriminator extends Agent{
+
+private int broj_batcheva=-1;
+	
+	private long lastModified = -1;
+	
+	private AID generator;
+	
+	private String saveLoc = "";
+	
+
+	@EJB
+	private Baza baza;
+
+	
+	@Override
+	public void stop() {
+		System.out.println("GAN STAO");
+	}
+
+
+	@Override
+	public void handleMessage(ACLPoruka poruka) {
+		boolean b = true;
+		
+		if(poruka.getPerformative().equals(Performative.STARTAI)) {
+			
+			broj_batcheva=(int)poruka.getContentObj();
+			
+			
+			
+			System.out.println("START GAN");
+			
+			ACLPoruka temp = new ACLPoruka();
+			
+			temp.setContent("POCNI GAN");
+			temp.setSender(this.getAid());
+			
+			
+			HashMap<AID, Agent> agenti = baza.getAgenti();
+			
+			ArrayList<AID> receivers = new ArrayList<>();
+			
+			for(Map.Entry<AID, Agent> entry : agenti.entrySet()) {
+				
+				AID key = entry.getKey();
+			    
+				Agent value = entry.getValue();
+
+				if(key.getType().getName().equals("GENERATOR"))
+					receivers.add(key);
+			 
+				if(receivers.size()==0) {
+					System.out.println("NEMA GENERATORA");
+					b=false;
+				}
+			}
+			
+			
+			if(b) {
+				
+				AID[] r = new AID[1];
+				r[0] = receivers.get(0);
+				generator=receivers.get(0);
+				
+				temp.setReceivers(r);
+				
+				temp.setProtocol((String)poruka.getUserArgs().get("DIS_LOC")); // OVDE CE BITI STRING ZA LOKACIJU PYa
+				temp.setConversationID(poruka.getConversationID());
+				temp.setPerformative(Performative.STARTAI);
+				temp.setOntology((String)poruka.getUserArgs().get("DIS_RES_LOC"));
+				temp.setLanguage((String)poruka.getUserArgs().get("DIS_SAVE_LOC"));
+				//DODAJ AKO NESTO FALI
+				
+				
+				ContractNetDTO cdto = new ContractNetDTO();
+				
+				ACLPoruka next = new ACLPoruka();
+				next.setSender(this.id);
+				next.setReceivers(new AID[] { this.id });
+				next.setPerformative(Performative.STARTGAN);
+				next.setProtocol((String)poruka.getUserArgs().get("GEN_LOC"));// OVDE CE BITI STRING ZA LOKACIJU PY-a
+				next.setOntology((String)poruka.getUserArgs().get("GEN_RES_LOC")); // LOKACIJA TEXT FILE-a
+				next.setLanguage((String)poruka.getUserArgs().get("GEN_SAVE_LOC")); // LOKACIJA FILE IZ KOJE PY CITA
+				saveLoc= (String)poruka.getUserArgs().get("GEN_SAVE_LOC");
+				
+				System.out.println("DALJE");
+				
+				new JMSQueue(next);
+				new JMSQueue(temp);
+				//da li sam trebao obe da saljem?
+		
+			}
+			
+			
+		}else if (poruka.getPerformative().equals(Performative.ENDGAN)) {
+			System.out.println("END GAN");
+		}else if (poruka.getPerformative().equals(Performative.RETURNRESULTGENERATOR)) {
+		
+			File f = new File(saveLoc);
+			try {
+				Files.deleteIfExists(f.toPath());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+			
+			File actualFile = new File ((File)poruka.getContentObj(), saveLoc);
+			
+			System.out.println("SACUVAO");
+			
+
+		}else if (poruka.getPerformative().equals(Performative.STARTGAN)) {
+			
+			System.out.println("STARTUJE GAN U GENERATORU");
+		
+			
+			String[] cmd = poruka.getProtocol().split(",");
+					
+					/*{
+			        "/bin/bash",
+			        "-c",
+			        "echo password | python script.py '" + packet.toString() + "'"
+			    };*/
+			try {
+				Runtime.getRuntime().exec(cmd);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		
+			ACLPoruka next = new ACLPoruka();
+			next.setSender(this.id);
+			next.setReceivers(new AID[] { this.id });
+			next.setPerformative(Performative.RETURNRESULTGENERATOR);
+			next.setOntology(poruka.getOntology());
+			next.setLanguage(poruka.getLanguage());
+	
+			System.out.println("POCEO GAN");
+			
+			
+		}else if (poruka.getPerformative().equals(Performative.RETURNRESULTGENERATOR)) {
+			
+			if(broj_batcheva==0) {
+				
+				ACLPoruka next = new ACLPoruka();
+				next.setSender(this.id);
+				next.setReceivers(new AID[] { this.id });
+				next.setPerformative(Performative.ENDGAN);
+				
+				
+				ACLPoruka next2 = new ACLPoruka();
+				next2.setSender(this.id);
+				next2.setReceivers(new AID[] { generator });
+				next2.setPerformative(Performative.ENDGAN);
+				System.out.println("KRAJ");
+				
+				
+				//GRBA SALJI PORUKE
+				new JMSQueue(next);
+				new JMSQueue(next2);
+			
+			}
+			
+			
+			try {
+				WatchService watcher = FileSystems.getDefault().newWatchService();
+				Path dir = Paths.get(poruka.getOntology());
+				WatchKey key = dir.register(watcher, StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_CREATE);
+				
+				boolean next = false;
+				while(true) {
+					
+					if(next)
+						break;
+					
+					for(WatchEvent<?> event : key.pollEvents()) {
+						
+						if(event.kind() == StandardWatchEventKinds.ENTRY_CREATE) {
+							System.out.println("TEK SE SADA KREIRAO FAJL");
+						}else {
+							
+
+							ACLPoruka next2 = new ACLPoruka();
+							next2.setSender(this.id);
+							next2.setReceivers(new AID[] { generator });
+							next2.setPerformative(Performative.ENDGAN);
+							next2.setContentObj(new File(poruka.getOntology())); // CEO FAJL DOBIJAS
+							System.out.println("SALJEM DALJE");
+							
+							//GRBA SALJI PORUKU
+							new JMSQueue(next2);
+							next= true; 
+							break;
+							
+						}
+						
+					}
+					
+				}
+				
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+				
+			
+		}
+		
+	}
+	
+}
