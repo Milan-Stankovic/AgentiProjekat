611843669dad28b074b528a661f2551f8f011da8
ilijagrbic <ilijagrb.ns@gmail.com>
2018-07-03 15:03:50 +0200

    gen

--- aiPy/generator.py
+++ aiPy/generator.py
@@ -2,11 +2,11 @@ import time
 
 def main():
   time.sleep(1)
-  f = open("output.txt", "w+")
+  f = open("output/output.txt", "w+")
   f.write("HELLO AGENT FROM GENERATOR !")
   f.close()
   time.sleep(10)
-  f = open("input.txt", "r")
+  f = open("input/input.txt", "r")
   text = f.read()
   f.close()
   print(text)
