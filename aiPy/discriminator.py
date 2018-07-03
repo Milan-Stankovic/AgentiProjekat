165af6642fc27440a440ada738ef0315c132ca81
Milan-Stankovic <milans995@gmail.com>
2018-07-03 14:49:59 +0200

    py ispravljen

--- aiPy/discriminator.py
+++ aiPy/discriminator.py
@@ -1,15 +1,13 @@
+import time
 def main():
-    
-	f = open("input.txt", "r")
-    text = f.read()
-    f.close()
-    print(text)
-	
-	
-	time.sleep(3)
-	f = open("output.txt", "w+")
-    f.write("HELLO AGENT FROM DISCIMINATOR !")
-    f.close()
+  f = open("input.txt", "r")
+  text = f.read()
+  f.close()
+  print(text)
+  time.sleep(3)
+  f = open("output.txt", "w+")
+  f.write("HELLO AGENT FROM DISCIMINATOR !")
+  f.close()
 	
 	
 if __name__== "__main__":
--- aiPy/generator.py
+++ aiPy/generator.py
@@ -1,17 +1,15 @@
 import time
 
 def main():
-	
-	time.sleep(1)
-	f = open("output.txt", "w+")
-    f.write("HELLO AGENT FROM GENERATOR !")
-    f.close()
-	
-	time.sleep(10)
-	f = open("input.txt", "r")
-    text = f.read()
-    f.close()
-    print(text)
+  time.sleep(1)
+  f = open("output.txt", "w+")
+  f.write("HELLO AGENT FROM GENERATOR !")
+  f.close()
+  time.sleep(10)
+  f = open("input.txt", "r")
+  text = f.read()
+  f.close()
+  print(text)
 	
 	
 
